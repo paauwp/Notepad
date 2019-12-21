d6e79616e4183926b3b50606eefdaf46460c4c16
Pieter Paauw <39484121+paauwp@users.noreply.github.com>
2019-12-22 00:48:52 +0100

    Add files via upload

--- notepad.py
+++ notepad.py
@@ -10,7 +10,7 @@ import os
 # Create the Notepad Class
 class Notepad:
     root = Tk()
-    #root.wm_iconbitmap("Users/HP/Documenten/notepad.ico")
+    root.wm_iconbitmap('C:/Python Scripts/notepad.ico')
     root.title("Untitled - Notepad")
     root.geometry("700x400")
     TextArea = Text(root, font = ("arial, 15"))
@@ -31,22 +31,21 @@ class Notepad:
         #File Nenu
         self.FileMenu.add_command(label = "New")
         self.FileMenu.add_command(label = "Save")
-        self.FileMenu.add_command(label = "Open")
-        self.FileMenu.add_command(label = "Exit")
-        
+        self.FileMenu.add_command(label = "Open", command=self.openFile)
+        self.FileMenu.add_separator()
+        self.FileMenu.add_command(label = "Exit", activebackground ="red", command=self.quitApp)
         self.menubar.add_cascade(label = "File", menu = self.FileMenu)
        
         #Edit Menu
         self.EditMenu.add_command(label = "Select All     CTRL-A")
-        self.EditMenu.add_command(label ="Cut     CTRL-X")
-        self.EditMenu.add_command(label ="Copy    CTRL-C")
-        self.EditMenu.add_command(label ="New")
-        self.EditMenu.add_command(label ="Paste     CTRL-V")
+        self.EditMenu.add_command(label ="Cut               CTRL-X")
+        self.EditMenu.add_command(label ="Copy            CTRL-C")
+        self.EditMenu.add_command(label ="Paste            CTRL-V")
         
         self.menubar.add_cascade(label = "File", menu = self.EditMenu)
         
         #Help Menu
-        self.HelpMenu.add_command(label = "About Notepad")
+        self.HelpMenu.add_command(label = "About Notepad", command=self.showAppInfo)
         self.menubar.add_cascade(label = "Help", menu = self.HelpMenu)
         
         #Config & Pack
@@ -55,6 +54,24 @@ class Notepad:
         self.Scrollbar.config(command=self.TextArea.yview)
         self.TextArea.config(yscrollcommand = self.Scrollbar.set)
 
+    def quitApp(self):
+        self.root.destroy()
+
+    def showAppInfo(self):
+        showinfo("Notepad", "This is the Notepad application")
+    
+    def openFile(self):
+        self.file = askopenfilename(defaultextension='.txt', filetypes = [('All Files', '*.*'), ('Text Document', '*.txt')])
+
+        if self.file == "":
+            self.file = None
+        else:
+            self.root.title(os.path.basename(self.file)+" - Notepad")
+            self.TextArea.delete(1.0, END)
+            file = open(self.file, "r")
+            self.TextArea.insert(1.0, file.read())
+            file.close()
+    
     def run(self):
         self.root.mainloop()
 
